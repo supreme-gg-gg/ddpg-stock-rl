d7662a8b22e3cd3db4602a1d07fe4186578a0e99
Jet Chiang <jetjiang.ez@gmail.com>
2025-02-21 15:59:33 -0500

    update training loop, refactor linear layers

--- src/model/base.py
+++ src/model/base.py
@@ -31,31 +31,3 @@ class PredictorBase(nn.Module, ABC):
     @abstractmethod
     def forward(self, x):
         raise NotImplementedError("Forward method must be implemented in subclass")
-    
-class FullyConnectedLayers(nn.Module):
-    """Final fully connected layers for the actor and critic"""
-    def __init__(self, input_dim, output_dim, use_batch_norm):
-        super().__init__()
-        self.fc1 = nn.Linear(input_dim, 64)
-        self.bn1 = nn.BatchNorm1d(64) if use_batch_norm else nn.Identity()
-        self.fc2 = nn.Linear(64, 64)
-        self.bn2 = nn.BatchNorm1d(64) if use_batch_norm else nn.Identity()
-        self.fc3 = nn.Linear(64, output_dim)
-        self.softmax = nn.Softmax(dim=-1)
-
-    def forward(self, x):
-        x = F.relu(self.bn1(self.fc1(x)))
-        x = F.relu(self.bn2(self.fc2(x)))
-        x = self.softmax(self.fc3(x))
-        return x
-
-class BaseNetwork(nn.Module, ABC):
-    """Abstract base class for Actor and Critic"""
-    def __init__(self, predictor: PredictorBase, fc_layers: FullyConnectedLayers):
-        super().__init__()
-        self.predictor = predictor
-        self.fc_layers = fc_layers
-
-    @abstractmethod
-    def forward(self, x):
-        raise NotImplementedError("Forward method must be implemented in subclass")
