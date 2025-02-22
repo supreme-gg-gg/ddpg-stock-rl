b6d55fe4142d3e4b8d790077c65d3a659dc99d6c
Jet Chiang <jetjiang.ez@gmail.com>
2025-02-21 23:59:33 -0500

    create ddpg training setup, minor refactoring

---
+++ src/ddpg_train.py
@@ -0,0 +1,87 @@
+"""
+Use DDPG to train a stock trader based on a window of history price
+"""
+
+from __future__ import print_function, division
+
+import numpy as np
+import argparse
+import pprint
+
+from model.ddpg.actor import StockActor
+from model.ddpg.critic import StockCritic
+from model.ddpg.ddpg import DDPGAgent
+from model.ddpg.ornstein_uhlenbeck import OrnsteinUhlenbeckActionNoise
+
+from environment.portfolio import PortfolioEnv
+from utils.data import read_stock_history, obs_normalizer
+from utils.helpers import get_model_path, get_result_path
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Provide arguments for training different DDPG models')
+
+    parser.add_argument('--debug', '-d', help='print debug statement', default=False)
+    parser.add_argument('--predictor_type', '-p', help='cnn or lstm predictor', required=True)
+    parser.add_argument('--window_length', '-w', help='observation window length', required=True)
+    parser.add_argument('--batch_norm', '-b', help='whether to use batch normalization', required=True)
+
+    args = vars(parser.parse_args())
+
+    pprint.pprint(args)
+
+    if args['debug'] == 'True':
+        DEBUG = True
+    else:
+        DEBUG = False
+
+    # load the data
+    history, abbreviation = read_stock_history(filepath='utils/datasets/stocks_history_target.h5')
+    history = history[:, :, :4]
+    target_stocks = abbreviation
+    num_training_time = 1095
+    window_length = int(args['window_length'])
+    nb_classes = len(target_stocks) + 1
+
+    # get target history
+    target_history = np.empty(shape=(len(target_stocks), num_training_time, history.shape[2]))
+    for i, stock in enumerate(target_stocks):
+        target_history[i] = history[abbreviation.index(stock), :num_training_time, :]
+    print(target_history.shape)
+
+    # setup environment
+    env = PortfolioEnv(target_history, target_stocks, steps=1000, window_length=window_length)
+
+    # do some setup and checking
+    action_dim = [nb_classes]
+    state_dim = [nb_classes, window_length]
+    batch_size = 64
+    action_bound = 1.
+    tau = 1e-3
+    assert args['predictor_type'] in ['cnn', 'lstm'], 'Predictor must be either cnn or lstm'
+    predictor_type = args['predictor_type']
+    if args['batch_norm'] == 'True':
+        use_batch_norm = True
+    elif args['batch_norm'] == 'False':
+        use_batch_norm = False
+    else:
+        raise ValueError('Unknown batch norm argument')
+    model_save_path = get_model_path(window_length, predictor_type, use_batch_norm)
+    summary_path = get_result_path(window_length, predictor_type, use_batch_norm)
+    
+    # create actor critic noise and agent
+    actor_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(action_dim))
+    actor = StockActor(state_dim, action_dim, action_bound, 1e-4, tau, batch_size,
+                        predictor_type, use_batch_norm)
+    critic = StockCritic(state_dim=state_dim, action_dim=action_dim, learning_rate=1e-3, tau=1e-3,
+                             predictor_type=predictor_type, use_batch_norm=use_batch_norm)
+    
+    # Initalize the model (no need to load weight unless using checkpoints)
+    ddpg_model = DDPGAgent(env, actor, critic, actor_noise, obs_normalizer=obs_normalizer,
+                        config_file='config/stock.json', model_save_path=model_save_path,
+                        summary_path=summary_path)
+    ddpg_model.train() # this saves automatically
+
+    # TODO: Check if action_processor is necessary, it is NOT used in the original code but
+    # seems to have support built-in in the networks.
+
---
+++ src/utils/helpers.py
@@ -0,0 +1,74 @@
+"""
+Helper functions migrated from the original stock_trading.py in src.
+This helps modularise usage to group all helper functions under utils.
+"""
+
+import numpy as np
+
+def get_model_path(window_length, predictor_type, use_batch_norm):
+    if use_batch_norm:
+        batch_norm_str = 'batch_norm'
+    else:
+        batch_norm_str = 'no_batch_norm'
+    return 'weights/stock/{}/window_{}/{}/checkpoint.ckpt'.format(predictor_type, window_length, batch_norm_str)
+
+
+def get_result_path(window_length, predictor_type, use_batch_norm):
+    if use_batch_norm:
+        batch_norm_str = 'batch_norm'
+    else:
+        batch_norm_str = 'no_batch_norm'
+    return 'results/stock/{}/window_{}/{}/'.format(predictor_type, window_length, batch_norm_str)
+
+# NOTE: This function is deprecated, variable scope is no longer used in PyTorch, this is old TensorFlow code
+# def get_variable_scope(window_length, predictor_type, use_batch_norm):
+#     if use_batch_norm:
+#         batch_norm_str = 'batch_norm'
+#     else:
+#         batch_norm_str = 'no_batch_norm'
+#     return '{}_window_{}_{}'.format(predictor_type, window_length, batch_norm_str)
+
+def test_model(env, model):
+    observation, info = env.reset()
+    done = False
+    while not done:
+        action = model.predict_single(observation)
+        observation, _, done, _ = env.step(action)
+    env.render()
+
+def test_model_multiple(env, models):
+    observation, info = env.reset()
+    done = False
+    while not done:
+        actions = []
+        for model in models:
+            actions.append(model.predict_single(observation))
+        actions = np.array(actions)
+        observation, _, done, info = env.step(actions)
+    env.render()
+
+def peek_stock_data():
+    """you should have h5py installed to preview the data"""
+    # Open the H5 file
+    import h5py
+    with h5py.File('utils/datasets/stocks_history_target_2.h5', 'r') as f:
+        # Print the keys in the file
+        print("Keys in the H5 file:", list(f.keys()))
+        
+        # Get the stock data and abbreviations
+        stock_data = f['history'][:]  # Assuming 'stock_data' is the dataset name
+        stock_abbr = f['abbreviation'][:] # Assuming 'abbreviation' is the dataset name
+        
+        # Convert byte strings to regular strings if needed
+        stock_abbr = [abbr.decode('utf-8') if isinstance(abbr, bytes) else abbr for abbr in stock_abbr]
+        
+        # Print the shape of the data
+        print("\nData shape:", stock_data.shape)
+        print("Stock abbreviations:", stock_abbr)
+        
+        # Show first few entries
+        print("\nFirst few entries of the stock data:")
+        # Assuming the data structure is [stocks, time_steps, features]
+        for i in range(min(5, len(stock_abbr))):
+            print(f"\n{stock_abbr[i]}:")
+            print(stock_data[i, :5])  # First 5 time steps
